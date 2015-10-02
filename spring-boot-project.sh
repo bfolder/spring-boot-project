e00706b08695dc426b1d4c67d97757cb43ab0886
Heiko Dreyer <mail@boxedfolder.com>
2015-10-02 12:07:50 +0200

    First commit - This should be it

---
+++ spring-boot-project.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+                    
+if [ $# -ne 2 ]
+  then
+    echo "Wrong number of arguments. First one is the name of your project, second one your package."    
+	exit 1
+fi
+
+echo Creating a spring boot project with name $1 and package $2
+packageFolder=$(echo "$2" | tr '.' '/')
+mkdir -p ./$1/src/main/java/$packageFolder
+
+# Writing pom           
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+
+    <groupId>org.springframework</groupId>
+    <artifactId>gs-spring-boot</artifactId>
+    <version>0.1.0</version>
+
+    <parent>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-parent</artifactId>
+        <version>1.2.6.RELEASE</version>
+    </parent>
+
+    <dependencies>
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-web</artifactId>
+        </dependency>
+    </dependencies>
+
+    <properties>
+        <java.version>1.8</java.version>
+    </properties>
+
+
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.springframework.boot</groupId>
+                <artifactId>spring-boot-maven-plugin</artifactId>
+            </plugin>
+        </plugins>
+    </build>
+
+</project>' > ./$1/pom.xml
+
+# Writing application class
+echo "
+package $2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}" > ./$1/src/main/java/$packageFolder/Application.java
+
+echo Done.
